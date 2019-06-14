6a88d3914d3567b50ef93de54a8ac67e9f6a751b
ntchuong97 <49951932+ntchuong97@users.noreply.github.com>
2019-06-14 22:03:57 +0700

    Chuong lan 3

--- app/src/main/java/com/teamphe/todocalendar/CalendarActivity.kt
+++ app/src/main/java/com/teamphe/todocalendar/CalendarActivity.kt
@@ -10,23 +10,32 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.CalendarView
-import android.widget.TextView
-import kotlin.Int
 import android.widget.Toast
-import com.teamphe.todocalendar.R
 import java.util.*
 
 class CalendarActivity : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val myView = inflater!!.inflate(R.layout.fragment_calendar, container, false)
-
+        val myView = inflater.inflate(R.layout.fragment_calendar, container, false)
+        //val setEvenBtn = myView.findViewById<Button>(R.id.btn_setEvent)
         val calendarView = myView.findViewById<CalendarView>(R.id.calendar)
+
         calendarView?.setOnDateChangeListener { view, year, month, dayOfMonth ->
             // Note that months are indexed from 0. So, 0 means January, 1 means february, 2 means march etc.
             val msg = "Selected date is " + dayOfMonth + "/" + (month + 1) + "/" + year
             Toast.makeText(this@CalendarActivity.context, msg, Toast.LENGTH_SHORT).show()
         }
+
+        /*setEvenBtn.setOnClickListener {
+            val intent = Intent(activity, CalendarContract.Calendars::class.java)
+                .setData(CONTENT_URI)
+                .putExtra(CalendarContract.Events.TITLE, "My Event")
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, "Here")
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, System.currentTimeMillis() + (60 * 60 * 1000))
+            activity!!.startActivity(intent)
+        }*/
+
         return myView
     }
 }
